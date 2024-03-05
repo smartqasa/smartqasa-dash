ecb3882941d68352ab60ac504f721459e1966da0
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-05 00:05:41 +0000

    Rename to Elements

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/smartqasa.js',
+    file: 'dist/elements.js',
     format: 'iife', // Using 'iife' for browsers
     name: 'SmartQasaElements',
   },
--- src/tiles/light-tile.js
+++ src/tiles/light-tile.js
@@ -21,7 +21,7 @@ export class SmartQasaLightTile extends LitElement {
       this._icon = config.icon || null;
       this._name = config.name || null;
     } else {
-      throw new Error('You need to define an entity');
+      throw new Error('You need to define an entity.');
     };
   }
 
