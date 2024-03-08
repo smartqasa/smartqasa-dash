f3461394caec5bc4f82b94dbd86840c3c825ea75
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-08 01:18:32 +0000

    Sizing debug

--- dist/elements.js
+++ dist/elements.js
@@ -56,7 +56,7 @@
         grid-area: n;
         place-self: center start;
         max-height: calc(var(--sq-primary-font-size) * 2);
-        max-width: fit-content;
+        max-width: 90%;
         text-align: left;
         overflow: hidden;
         text-overflow: ellipsis;
