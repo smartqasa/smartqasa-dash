dea461a37d5845671d0cebfc57797523c2a95456
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-21 09:48:38 +0000

    More on TS

--- src/index.js
+++ src/index.js
@@ -9,22 +9,4 @@ window.smartqasa.deviceType =
 window.customCards = window.customCards ?? [];
 
 // Tiles
-import { SmartQasaAllOffTile } from "./tiles/all-off";
-import { SmartQasaAppTile } from "./tiles/app";
-import { SmartQasaAreaTile } from "./tiles/area";
-import { SmartQasaFanTile } from "./tiles/fan";
-import { SmartQasaGarageTile } from "./tiles/garage";
 import { SmartQasaLightTile } from "./tiles/light";
-import { SmartQasaLockTile } from "./tiles/lock";
-import { SmartQasaRoutineTile } from "./tiles/routine";
-import { SmartQasaShadeTile } from "./tiles/shade";
-import { SmartQasaSwitchTile } from "./tiles/switch";
-
-// Chips
-import { SmartQasaMotionChip } from "./chips/motion";
-import { SmartQasaNavigateChip } from "./chips/navigate";
-import { SmartQasaThermostatChip } from "./chips/thermostat";
-
-// Misc
-import { SmartQasaTimeDate } from "./misc/time-date";
-import { SmartQasaAreaPicture } from "./misc/area-picture";
