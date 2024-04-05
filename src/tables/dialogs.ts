968f1a6576d5de36ead549786ac16a148bf0f4c9
Tom Bruno <tom.bruno@smartqasa.com>
2024-04-05 18:19:35 +0000

    Add new dialog tiles for locks and thermostats

--- src/tables/dialogs.ts
+++ src/tables/dialogs.ts
@@ -62,6 +62,78 @@ const dialogTable: DialogTable = {
       }
     }
   },
+  locks: {
+    icon: "hass:lock",
+    name: "Door Locks",
+    data: {
+      title: "Door Locks",
+      size: "normal",
+      timeout: 60000,
+      content: {
+        type: "custom:auto-entities",
+        card: {
+          type: "custom:layout-card",
+          layout_type: "custom:grid-layout",
+          layout: {
+            "margin": 0,
+            "grid-template-columns": "1fr",
+            "grid-gap": "var(--sq-dialog-grid-gap)"
+          }
+        },
+        card_param: "cards",
+        filter: {
+          include: [
+            {
+              group: "lock.all_door_locks",
+              sort: {
+                method: "friendly_name",
+                ignore_case: true
+              },
+              options: {
+                type: "custom:smartqasa-lock-tile"
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  thermostats: {
+    icon: "hass:thermometer-lines",
+    name: "Thermostats",
+    data: {
+      title: "Thermostats",
+      size: "normal",
+      timeout: 60000,
+      content: {
+        type: "custom:auto-entities",
+        card: {
+          type: "custom:layout-card",
+          layout_type: "custom:grid-layout",
+          layout: {
+            "margin": 0,
+            "grid-template-columns": "1fr",
+            "grid-gap": "var(--sq-dialog-grid-gap)"
+          }
+        },
+        card_param: "cards",
+        filter: {
+          include: [
+            {
+              domain: "climate",
+              sort: {
+                method: "friendly_name",
+                ignore_case: true
+              },
+              options: {
+                type: "custom:smartqasa-thermostat-tile"
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
 };
 
 export default dialogTable;
