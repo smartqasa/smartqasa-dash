2b461248709c6f32a9c777452a850d41a36c2e7c
Tom Bruno <tom.bruno@smartqasa.com>
2024-04-06 21:32:14 +0000

    Update dialogTable layout properties for sensors in elements.js and dialogs.ts

--- src/tables/dialogs.ts
+++ src/tables/dialogs.ts
@@ -203,13 +203,48 @@ const dialogTable: DialogTable = {
         filter: {
           include: [
             {
-              group: "lock.all_door_sensors",
+              group: "binary_sensor.all_door_sensors",
               sort: {
                 method: "friendly_name",
                 ignore_case: true
               },
               options: {
-                type: "custom:smartqasa-lock-tile"
+                type: "custom:smartqasa-sensor-tile"
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  sensors_windows: {
+    icon: "hass:window-open",
+    name: "Window Sensors",
+    data: {
+      title: "Window Sensors",
+      timeout: 60000,
+      content: {
+        type: "custom:auto-entities",
+        card: {
+          type: "custom:layout-card",
+          layout_type: "custom:grid-layout",
+          layout: {
+            "margin": 0,
+            "grid-template-columns": "1fr",
+            "grid-gap": "var(--sq-dialog-grid-gap)"
+          }
+        },
+        card_param: "cards",
+        filter: {
+          include: [
+            {
+              group: "binary_sensor.all_window_sensors",
+              sort: {
+                method: "friendly_name",
+                ignore_case: true
+              },
+              options: {
+                type: "custom:smartqasa-sensor-tile"
               }
             }
           ]
