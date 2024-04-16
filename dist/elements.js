1d9f9679b454abe14db60d515ba8fe5be03ae4b6
Tom Bruno <tom.bruno@smartqasa.com>
2024-04-16 22:15:24 +0000

    Update grid-template-columns in areasDialog and menuDialog to be responsive

--- dist/elements.js
+++ dist/elements.js
@@ -4691,7 +4691,7 @@ let PanelFooter = class PanelFooter extends s {
         }
         .grid {
             display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+            grid-template-columns: repeat(4, min-content);
             grid-gap: 5vw;
             justify-content: center;
             align-items: center;
--- src/misc/panel-footer.ts
+++ src/misc/panel-footer.ts
@@ -51,7 +51,7 @@ class PanelFooter extends LitElement implements ActionHandlers {
         }
         .grid {
             display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+            grid-template-columns: repeat(4, min-content);
             grid-gap: 5vw;
             justify-content: center;
             align-items: center;
