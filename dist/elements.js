4a988df21f0f66539d4d29572fa1042b29d51554
Tom Bruno <tom.bruno@smartqasa.com>
2024-04-29 11:49:07 +0000

    Update version to 1.1.90 in elements.js and package.json

--- dist/elements.js
+++ dist/elements.js
@@ -8494,7 +8494,7 @@ window.customCards.push({
     description: "A SmartQasa tile for controlling a thermostat climate entity.",
 });
 
-var version = "1.1.89";
+var version = "1.1.90";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
