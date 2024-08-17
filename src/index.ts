591ab1c5808f9ad42ff6c7636f03ff4944fa9a4c
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-17 11:34:21 +0000

    Refactor code to update version number to "2024.8.17b-1"

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,6 @@ window.customCards = window.customCards ?? [];
 import "./cards/panel";
 import "./cards/vertical-stack";
 import "./cards/tv-remote";
-import "./cards/tv-remote-v1";
 
 // Chips
 import "./chips/custom";
