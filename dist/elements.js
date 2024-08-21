4599ec229322a32d31fbe8ae5d28939aad707583
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-21 00:28:14 +0000

    Refactor code to update version number to "2024.8.20b-1" and align chips to the right

--- dist/elements.js
+++ dist/elements.js
@@ -9674,7 +9674,7 @@ PopupConfirmation = __decorate([
     t$1("popup-confirmation")
 ], PopupConfirmation);
 
-var version = "2024.8.19b-6";
+var version = "2024.8.20b-1";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
