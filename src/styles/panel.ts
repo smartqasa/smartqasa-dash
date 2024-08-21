a24224762024b9fb19a9ed1956fd42c83fde750c
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-21 16:11:12 +0000

    Refactor time and date display in panel card styles

--- src/styles/panel.ts
+++ src/styles/panel.ts
@@ -22,11 +22,13 @@ export const panelStyle = css`
     }
     .time,
     .date {
+        display: flex;
         text-align: left;
         line-height: normal;
         white-space: nowrap;
     }
     .time {
+        align-items: flex-start;
         font-size: var(--sq-title-font-size, 3.2rem);
         font-weight: var(--sq-title-font-weight, 400);
         color: rgb(var(--sq-title-font-rgb, 128, 128, 128));
