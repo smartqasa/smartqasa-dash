376ce2d792a08c0108382701ff3110689298fbd4
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-22 22:18:15 +0000

    Refactor panel card to use lazy loading for images and update version number

--- dist/elements.js
+++ dist/elements.js
@@ -14152,7 +14152,7 @@ PopupConfirmation = __decorate([
     t$1("popup-confirmation")
 ], PopupConfirmation);
 
-var version = "2024.8.22b-8";
+var version = "2024.8.22b-9";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
