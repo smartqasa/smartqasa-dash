e3dbf7555537e23ee455e0534e9b30785f41b794
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-23 11:19:11 +0000

    Refactor panel card to use Swiper for improved navigation

--- src/cards/panel.ts
+++ src/cards/panel.ts
@@ -4,6 +4,7 @@ import { styleMap } from "lit/directives/style-map.js";
 import { HassArea, HomeAssistant, LovelaceCard, LovelaceCardConfig } from "../types";
 import Swiper from "swiper";
 import { SwiperOptions } from "swiper/types";
+import { Navigation } from "swiper/modules";
 import { deviceType } from "../const";
 import { createElement } from "../utils/create-element";
 import { loadYamlAsJson } from "../utils/load-yaml-as-json";
@@ -180,12 +181,7 @@ export class PanelCard extends LitElement {
 
     private _initializeSwiper() {
         const swiperContainer = this.shadowRoot?.querySelector(".swiper");
-        if (!swiperContainer) {
-            console.error("Swiper container not found!");
-            return;
-        }
-
-        console.log("Initializing Swiper...");
+        if (!swiperContainer) return;
 
         const swiperParams: SwiperOptions = {
             navigation: {
@@ -195,9 +191,9 @@ export class PanelCard extends LitElement {
             initialSlide: 0,
         };
 
-        this._swiper = new Swiper(swiperContainer as HTMLElement, swiperParams);
+        Swiper.use([Navigation]);
 
-        console.log("Swiper initialized:", this._swiper);
+        this._swiper = new Swiper(swiperContainer as HTMLElement, swiperParams);
     }
 
     private async _loadContent() {
