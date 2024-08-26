41ce2ad4261b3f3068cc86cdc049df4a6af32250
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-26 18:13:30 +0000

    chore: Update version to 2024.8.26b-2

--- dist/elements.js
+++ dist/elements.js
@@ -14528,7 +14528,7 @@ PopupConfirmation = __decorate([
     t$1("popup-confirmation")
 ], PopupConfirmation);
 
-var version = "2024.8.26b-1";
+var version = "2024.8.26b-2";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
