5d066e6b508dac91b7c9cf5f5d3a8ce148daf510
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-27 02:19:19 +0000

    Refactor panel card for improved responsiveness and update container structure

--- dist/elements.js
+++ dist/elements.js
@@ -14538,7 +14538,7 @@ PopupConfirmation = __decorate([
     t$1("popup-confirmation")
 ], PopupConfirmation);
 
-var version = "2024.8.26b-4";
+var version = "2024.8.26b-5";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
