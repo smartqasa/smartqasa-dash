f2189fde76ee85df9825501d07044546f9f9e336
Tom Bruno <tom.bruno@smartqasa.com>
2024-08-29 15:14:37 +0000

    chore: Bump version to 2024.8.29b-2 and update package.json

--- dist/elements.js
+++ dist/elements.js
@@ -14714,7 +14714,7 @@ PopupConfirmation = __decorate([
     t$1("popup-confirmation")
 ], PopupConfirmation);
 
-var version = "2024.8.29b-1";
+var version = "2024.8.29b-2";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
