cb170f5eae2d364ed08efe72db4e7a65681a37e2
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-06 10:18:24 +0000

    Refactor import in light.ts to use entity-list-dialog-test instead of entity-list-dialog

--- src/tiles/light.ts
+++ src/tiles/light.ts
@@ -5,7 +5,7 @@ import { styleMap } from "lit/directives/style-map.js";
 import { HassEntity, HomeAssistant, LovelaceCardConfig } from "../types";
 import { callService } from "../utils/call-service";
 import { moreInfoDialog } from "../utils/more-info-dialog";
-import { entityListDialog } from "../utils/entity-list-dialog";
+import { entityListDialog } from "../utils/entity-list-dialog-test";
 
 import tileBaseStyle from "../styles/tile-base.css";
 import tileStateStyle from "../styles/tile-state.css";
