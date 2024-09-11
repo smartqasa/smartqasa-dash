b15541344728e34e246e40ad57ab76634b81df3d
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-11 18:11:57 +0000

    Refactor container class selector and update tablet portrait height in PanelCard component

--- src/cards/panel.ts
+++ src/cards/panel.ts
@@ -173,7 +173,7 @@ export class PanelCard extends LitElement {
 
         // prettier-ignore
         return html`
-        <div class="container ${classMap(classes)}">
+        <div class="container" admin control>
             ${this._deviceType === "tablet" ? this._renderHeader() : nothing}
             ${content}
             ${isPhoneLandscape ? nothing : this._renderFooter()}
