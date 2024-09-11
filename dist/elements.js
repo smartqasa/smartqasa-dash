b0affdfb53dd901b8fbd80e1078682f9b8b34243
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-11 17:03:07 +0000

    Bump version to 2024.9.11b-2 in package.json

--- dist/elements.js
+++ dist/elements.js
@@ -14743,7 +14743,7 @@ PopupDialog = __decorate([
     t$1("smartqasa-popup-dialog")
 ], PopupDialog);
 
-var version = "2024.9.11b-1";
+var version = "2024.9.11b-2";
 
 window.smartqasa = window.smartqasa || {};
 window.smartqasa.homePath = window.smartqasa.homePath || location.pathname.split("/").pop();
