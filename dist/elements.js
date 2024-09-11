c6f0267c6a4f30910ee5239b4894237d11d38f94
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-11 18:36:41 +0000

    Refactor container class selector and fix tablet portrait height in PanelCard component

--- dist/elements.js
+++ dist/elements.js
@@ -9365,7 +9365,7 @@ let PanelCard = class PanelCard extends h {
         }
         // prettier-ignore
         return ke `
-        <div class="container" ${Rt(classes)} >
+        <div class="container ${Rt(classes)}">
             ${this._deviceType === "tablet" ? this._renderHeader() : D}
             ${content}
             ${isPhoneLandscape ? D : this._renderFooter()}
