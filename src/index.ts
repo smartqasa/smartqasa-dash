d937ef991c46798e7edc596ab375e51ee6b99ee5
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-14 18:48:19 +0000

    Refactor control loading and rendering logic, and preload chipsConfig

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { LovelaceCardConfig } from "./types";
 import { version } from "../package.json";
 
 async function init() {
+    // Initialize smartqasa globals
     window.smartqasa = window.smartqasa || {};
     window.smartqasa.viewMode = "control";
     window.smartqasa.startArea = window.smartqasa.startArea || location.pathname.split("/").pop();
 
     window.customCards = window.customCards ?? [];
 
+    // Preload chips config
     try {
         const yamlFilePath = "/local/smartqasa/config/chips.yaml";
         const chipsConfig = await loadYamlAsJson<LovelaceCardConfig[]>(yamlFilePath);
@@ -18,57 +20,62 @@ async function init() {
         window.smartqasa.chipsConfig = [];
     }
 
-    // Panel
-    import("./panel/panel");
+    // Dynamically load Panel and other components
+    await import("./panel/panel");
 
-    // Cards
-    import("./cards/areas");
-    import("./cards/grid-stack");
-    import("./cards/group-stack");
-    import("./cards/horizontal-stack");
-    import("./cards/menu");
-    import("./cards/more-info");
-    import("./cards/pin-verify");
-    import("./cards/screensaver");
-    import("./cards/vertical-stack");
-    import("./cards/tv-remote");
-    import("./cards/weather");
+    // Load cards and chips in parallel
+    await Promise.all([
+        // Cards
+        import("./cards/areas"),
+        import("./cards/grid-stack"),
+        import("./cards/group-stack"),
+        import("./cards/horizontal-stack"),
+        import("./cards/menu"),
+        import("./cards/more-info"),
+        import("./cards/pin-verify"),
+        import("./cards/screensaver"),
+        import("./cards/vertical-stack"),
+        import("./cards/tv-remote"),
+        import("./cards/weather"),
 
-    // Chips
-    import("./chips/admin");
-    import("./chips/custom");
-    import("./chips/dialog");
-    import("./chips/motion");
-    import("./chips/navigate");
-    import("./chips/routine");
-    import("./chips/select");
-    import("./chips/thermostat");
-    import("./chips/weather");
+        // Chips
+        import("./chips/admin"),
+        import("./chips/custom"),
+        import("./chips/dialog"),
+        import("./chips/motion"),
+        import("./chips/navigate"),
+        import("./chips/routine"),
+        import("./chips/select"),
+        import("./chips/thermostat"),
+        import("./chips/weather"),
+    ]);
 
-    // Tiles
-    import("./tiles/action");
-    import("./tiles/all-off");
-    import("./tiles/app");
-    import("./tiles/area");
-    import("./tiles/dialog");
-    import("./tiles/fan");
-    import("./tiles/garage");
-    import("./tiles/heater");
-    import("./tiles/light");
-    import("./tiles/light-editor");
-    import("./tiles/lock");
-    import("./tiles/option");
-    import("./tiles/robot");
-    import("./tiles/roku");
-    import("./tiles/routine");
-    import("./tiles/select");
-    import("./tiles/sensor");
-    import("./tiles/pool-light");
-    import("./tiles/pool-light-sequencer");
-    import("./tiles/shade");
-    import("./tiles/switch");
-    import("./tiles/theme");
-    import("./tiles/thermostat");
+    // Load tiles in parallel
+    await Promise.all([
+        import("./tiles/action"),
+        import("./tiles/all-off"),
+        import("./tiles/app"),
+        import("./tiles/area"),
+        import("./tiles/dialog"),
+        import("./tiles/fan"),
+        import("./tiles/garage"),
+        import("./tiles/heater"),
+        import("./tiles/light"),
+        import("./tiles/light-editor"),
+        import("./tiles/lock"),
+        import("./tiles/option"),
+        import("./tiles/robot"),
+        import("./tiles/roku"),
+        import("./tiles/routine"),
+        import("./tiles/select"),
+        import("./tiles/sensor"),
+        import("./tiles/pool-light"),
+        import("./tiles/pool-light-sequencer"),
+        import("./tiles/shade"),
+        import("./tiles/switch"),
+        import("./tiles/theme"),
+        import("./tiles/thermostat"),
+    ]);
 
     // Log version info
     console.info(`%c SmartQasa ⏏ ${version} `, "background-color: #0000ff; color: #ffffff; font-weight: 700;");
