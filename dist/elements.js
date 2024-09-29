f486ac02a1ed0f63b31927053335abdde4b85bb3
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-29 21:16:07 +0000

    Refactor launchApp utility function and update import paths

--- dist/elements.js
+++ dist/elements.js
@@ -15186,7 +15186,7 @@ var thermostat = /*#__PURE__*/Object.freeze({
   get ThermostatTile () { return ThermostatTile; }
 });
 
-var version = "2024.9.26b-1";
+var version = "2024.9.29b-1";
 
 var _package = /*#__PURE__*/Object.freeze({
   __proto__: null,
