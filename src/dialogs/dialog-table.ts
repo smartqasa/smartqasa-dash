ea981a12796ffcf690202dbf562b7c63fb3696de
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-17 11:39:09 -0500

    Update timestamp and adjust icon styles for dialog table

--- src/dialogs/dialog-table.ts
+++ src/dialogs/dialog-table.ts
@@ -401,7 +401,7 @@ export const dialogTable: DialogTable = {
                     style: {
                         'mxmp-player$ mxmp-player-controls$': `
                             .icons {
-                                margin-bottom: 1rem;
+                                gap: 1rem;
                             }
                             mxmp-ha-player, ha-icon-button {
                                 --mdc-icon-size: 2rem !important;
@@ -410,6 +410,8 @@ export const dialogTable: DialogTable = {
                                 background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
                             }
                             .big-icon {
+                                --mdc-icon-size: 3rem !important;
+                                --mdc-icon-button-size: 4rem !important;
                                 border-radius: 50%;
                                 background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
                             }
