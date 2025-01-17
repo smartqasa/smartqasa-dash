2b500cb9d0bc77f400247d5b62c73ef02f2737eb
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-17 12:39:13 -0500

    Update injected timestamp and refactor media player card styles

--- src/dialogs/media-player-style.ts
+++ src/dialogs/media-player-style.ts
@@ -1,26 +1,30 @@
 export const mediaPlayerCardMod = {
     style: `
         mxmp-player$ {
-            mxmp-player-header$ .entity {
-                color: rgb(var(--sq-primary-font-rgb));
-                font-weight: var(--sq-primary-font-weight);
-                font-size: var(--sq-primary-font-size);
+            mxmp-player-header$ {
+                .entity {
+                    color: rgb(var(--sq-primary-font-rgb));
+                    font-weight: var(--sq-primary-font-weight);
+                    font-size: var(--sq-primary-font-size);
+                }
             }
-            mxmp-player-controls$ .icons {
-                margin-bottom: 1rem;
-                gap: 1rem;
-            }
-            mxmp-player-controls$ mxmp-ha-player, mxmp-player-controls$ ha-icon-button {
-                --mdc-icon-size: 2rem !important;
-                --mdc-icon-button-size: 3rem !important;
-                border-radius: 50%;
-                background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
-            }
-            mxmp-player-controls$ .big-icon {
-                --mdc-icon-size: 3rem !important;
-                --mdc-icon-button-size: 4rem !important;
-                border-radius: 50%;
-                background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
+            mxmp-player-controls$ {
+                .icons {
+                    margin-bottom: 1rem;
+                    gap: 1rem;
+                }
+                mxmp-ha-player, ha-icon-button {
+                    --mdc-icon-size: 2rem !important;
+                    --mdc-icon-button-size: 3rem !important;
+                    border-radius: 50%;
+                    background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
+                }
+                .big-icon {
+                    --mdc-icon-size: 3rem !important;
+                    --mdc-icon-button-size: 4rem !important;
+                    border-radius: 50%;
+                    background-color: rgba(var(--sq-inactive-rgb), var(--sq-icon-opacity));
+                }
             }
         }
     `,
